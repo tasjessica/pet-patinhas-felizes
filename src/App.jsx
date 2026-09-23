e41177daab3eace9602a69f75bde20b5f6ad7786
Jess <tassjessica@gmail.com>
2026-09-23 17:16:57 -0300

    feat: adiciona seção hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,42 @@ import "./App.css";
 
 function App() {
   return (
-    <header>
-      <div className="logo">
-        <p>Patinhas Felizes</p>
-      </div>
+    <>
+      <header>
+        <div className="logo">
+          <p>Patinhas Felizes</p>
+        </div>
 
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Serviços</a>
-          </li>
-          <li>
-            <a href="#">Produtos</a>
-          </li>
-          <li>
-            <a href="#">Sobre</a>
-          </li>
-        </ul>
-      </nav>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Serviços</a>
+            </li>
+            <li>
+              <a href="#">Produtos</a>
+            </li>
+            <li>
+              <a href="#">Sobre</a>
+            </li>
+          </ul>
+        </nav>
 
-      <a href="#" className="botao-agendar">
-        Agendar
-      </a>
-    </header>
+        <a href="#" className="botao-agendar">
+          Agendar
+        </a>
+      </header>
+
+      <section className="hero">
+        <h1>Cuidado e carinho para o seu melhor amigo</h1>
+        <p>
+          Serviços de banho, tosa e muito mais, com todo carinho que seu pet
+          merece.
+        </p>
+        <a href="#" className="botao-agendar">
+          Agende uma visita
+        </a>
+      </section>
+    </>
   );
 }
 
